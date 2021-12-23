2da52867154eaece4d7f437a1812b9b74fafbf56
108703017 <108703017@nccu.edu.tw>
2021-12-23 22:35:40 +0800

    change delete constraint from cascade to restrict

--- project/create.sql
+++ project/create.sql
@@ -37,7 +37,7 @@ CREATE TABLE ObsWeather(
 	PRES float ,
 	StationNum varchar(256) not null,
 	primary key(ObsTime,StationNum),
-	CONSTRAINT fk_station FOREIGN KEY(StationNum) references Station(StationID) on delete cascade on update cascade,
+	CONSTRAINT fk_station FOREIGN KEY(StationNum) references Station(StationID) on delete restrict on update cascade,
 	CONSTRAINT fk_DateTime FOREIGN KEY(ObsTime) references DateTime(InfoTime) on update cascade
 );
 
@@ -49,7 +49,7 @@ create table Note(
 	N_StationID varchar(256) not null,
 	N_Time timestamp not null,
 	primary key(N_StationID,N_Time),
-	CONSTRAINT fk_station FOREIGN KEY(N_StationID) references Station(StationID) on delete cascade on update cascade,
+	CONSTRAINT fk_station FOREIGN KEY(N_StationID) references Station(StationID) on delete restrict on update cascade,
 	constraint fk_datetime FOREIGN KEY(N_Time) references DateTime(Infotime) on update cascade
 );
 
@@ -61,5 +61,5 @@ create table Record(
 	R_Date date not NULL,
 	R_StationID varchar(256) not NULL,
 	primary key(R_StationID,R_Date),
-	CONSTRAINT fk_station FOREIGN KEY(R_StationID) references Station(StationID) on delete cascade on update cascade
+	CONSTRAINT fk_station FOREIGN KEY(R_StationID) references Station(StationID) on delete restrict on update cascade
 );
