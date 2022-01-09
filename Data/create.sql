fa79bf4fa5d963f3268621b2fb6b3efaa15fd3c8
kebwlmbhee <yhqchiu@gmail.com>
2022-01-09 20:06:29 +0800

    fix uppercase

--- Data/create.sql
+++ Data/create.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Location(
+CREATE TABLE location(
 	City varchar(256) NOT NULL,
 	CitySn int NOT NULL,
 	Town varchar(256) NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE Location(
 	PRIMARY KEY(City,Town)
 );
 
-Create table Station(
+Create table station(
 	Lat float not null,
 	Lon float not null,
 	LatWGS84 float not null,
@@ -18,17 +18,17 @@ Create table Station(
 	S_Town varchar(256) not null,
 	CONSTRAINT PFK 
        FOREIGN KEY (S_City, S_Town) 
-       REFERENCES Location ( City, Town) on update cascade 
+       REFERENCES location ( City, Town) on update cascade 
 );
 
 
-create table DateTime(
+create table datetime(
 	InfoDate date not null,
 	InfoTime timestamp primary key
 );
 
 
-CREATE TABLE ObsWeather(
+CREATE TABLE obsweather(
 	ObsTime timestamp not null,
 	WDIR float,
 	WDSD float ,
@@ -37,29 +37,29 @@ CREATE TABLE ObsWeather(
 	PRES float ,
 	StationNum varchar(256) not null,
 	primary key(ObsTime,StationNum),
-	CONSTRAINT fk_station_weather FOREIGN KEY(StationNum) references Station(StationID) on delete restrict on update cascade,
-	CONSTRAINT fk_DateTime_weather FOREIGN KEY(ObsTime) references DateTime(InfoTime) on update cascade
+	CONSTRAINT fk_station_weather FOREIGN KEY(StationNum) references station(StationID) on delete restrict on update cascade,
+	CONSTRAINT fk_DateTime_weather FOREIGN KEY(ObsTime) references datetime(InfoTime) on update cascade
 );
 
 
 
-create table Note(
+create table note(
 	H_FX float,
 	H_XD float,
 	N_StationID varchar(256) not null,
 	N_Time timestamp not null,
 	primary key(N_StationID,N_Time),
-	CONSTRAINT fk_station_note FOREIGN KEY(N_StationID) references Station(StationID) on delete restrict on update cascade,
-	constraint fk_datetime_note FOREIGN KEY(N_Time) references DateTime(Infotime) on update cascade
+	CONSTRAINT fk_station_note FOREIGN KEY(N_StationID) references station(StationID) on delete restrict on update cascade,
+	constraint fk_datetime_note FOREIGN KEY(N_Time) references datetime(Infotime) on update cascade
 );
 
 
-create table Record(
+create table record(
 	D_TX float,
 	D_TN float,
 	H24R float,
 	R_Date date not NULL,
 	R_StationID varchar(256) not NULL,
 	primary key(R_StationID,R_Date),
-	CONSTRAINT fk_station_record FOREIGN KEY(R_StationID) references Station(StationID) on delete restrict on update cascade
+	CONSTRAINT fk_station_record FOREIGN KEY(R_StationID) references station(StationID) on delete restrict on update cascade
 );
